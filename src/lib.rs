cea77ebf2dd72faba97adb9293fd98822e024d3c
Andras Toth <eudrto@gmail.com>
2024-09-04 11:46:13 +0200

    refactor: move program’s logic to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use serde_json;
+use std::env;
+
+fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+    if encoded_value.chars().next().unwrap().is_digit(10) {
+        let colon_index = encoded_value.find(':').unwrap();
+        let number_string = &encoded_value[..colon_index];
+        let number = number_string.parse::<i64>().unwrap();
+        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
+        return serde_json::Value::String(string.to_string());
+    } else {
+        panic!("Unhandled encoded value: {}", encoded_value)
+    }
+}
+
+pub fn run() {
+    let args: Vec<String> = env::args().collect();
+    let command = &args[1];
+
+    if command == "decode" {
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value);
+        println!("{}", decoded_value.to_string());
+    } else {
+        println!("unknown command: {}", args[1])
+    }
+}
