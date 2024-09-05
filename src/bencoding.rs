7297e13383d311c1fa586df901693818066d54b4
Andras Toth <eudrto@gmail.com>
2024-09-05 20:54:55 +0200

    redesign bencoding parser

---
+++ src/bencoding.rs
@@ -0,0 +1,99 @@
+use serde_json::json;
+use std::fmt::Display;
+
+fn peek(bytes: &str) -> char {
+    bytes.chars().next().unwrap()
+}
+
+#[cfg(test)]
+fn read(window: &mut &str) -> char {
+    let result = peek(window);
+    *window = &window[result.len_utf8()..];
+    result
+}
+
+fn read_range<'a>(window: &mut &'a str, len: usize) -> &'a str {
+    let result = &window[..len];
+    *window = &window[len..];
+    result
+}
+
+fn read_until<'a>(window: &mut &'a str, ch: char) -> &'a str {
+    let colon_index = window.find(ch).unwrap();
+    let result = &window[..colon_index];
+    *window = &window[colon_index + 1..];
+    result
+}
+
+pub enum BValue<'a> {
+    String(&'a str),
+}
+
+impl<'a> BValue<'a> {
+    pub fn parse(mut encoded: &'a str) -> Self {
+        let window = &mut encoded;
+        Self::decode(window)
+    }
+
+    fn decode(window: &mut &'a str) -> BValue<'a> {
+        match peek(*window) {
+            c if c.is_ascii_digit() => {
+                let len = read_until(window, ':').parse::<usize>().unwrap();
+                BValue::String(read_range(window, len))
+            }
+            _ => {
+                unimplemented!()
+            }
+        }
+    }
+}
+
+impl<'a> Display for BValue<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::String(string) => {
+                write!(f, "{}", json!(string))
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_bencode;
+
+    #[test]
+    fn test_read() {
+        let mut val = "Äpfel";
+        let window = &mut val;
+        let char = read(window);
+        assert_eq!(char, 'Ä');
+        assert_eq!(*window, "pfel");
+    }
+
+    #[test]
+    fn test_string_single_digit_len() {
+        let val = "apple";
+        let encoded = serde_bencode::to_string(&val).unwrap();
+        let decoded = BValue::parse(&encoded);
+        assert_eq!(decoded.to_string(), json!(val).to_string());
+    }
+
+    #[test]
+    fn test_string_multi_digit_len() {
+        let val = "watermelon";
+        let encoded = serde_bencode::to_string(&val).unwrap();
+        let decoded = BValue::parse(&encoded);
+        assert_eq!(decoded.to_string(), json!(val).to_string());
+    }
+
+    #[test]
+    fn test_string_utf8() {
+        let val = "naïve";
+        let encoded = serde_bencode::to_string(&val).unwrap();
+        assert_eq!(read(&mut &encoded[..]), '6');
+        let decoded = BValue::parse(&encoded);
+        assert_eq!(decoded.to_string(), json!(val).to_string());
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,17 @@
+mod bencoding;
 mod cli;
 
+use bencoding::BValue;
 use clap::Parser;
 use cli::{Cli, SCommand};
-use serde_json;
-
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
-    }
-}
 
 pub fn run() {
     let cli = Cli::parse();
 
     match cli.s_command {
         SCommand::Decode { bencoded_value } => {
-            let decoded_value = decode_bencoded_value(&bencoded_value);
-            println!("{}", decoded_value.to_string());
+            let bval = BValue::parse(&bencoded_value);
+            println!("{}", bval);
         }
     }
 }
