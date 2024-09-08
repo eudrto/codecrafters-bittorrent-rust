5a09df63674347da3d77e1bf9fdc93aa2fb7cc3d
Andras Toth <eudrto@gmail.com>
2024-09-08 13:32:54 +0200

    improve the ergonomy of BString by implementing some obvious conversions

--- src/bstring.rs
+++ src/bstring.rs
@@ -1,13 +1,32 @@
-use std::str::from_utf8;
+use std::str::{from_utf8, Utf8Error};
 
 use serde::{Serialize, Serializer};
 
 #[derive(PartialEq, Eq, Hash)]
 pub struct BString<'a>(&'a [u8]);
 
+impl<'a> BString<'a> {
+    fn to_hex_string(&self) -> String {
+        self.0.iter().map(|byte| format!("{:02x}", byte)).collect()
+    }
+}
+
 impl<'a> From<&'a [u8]> for BString<'a> {
     fn from(value: &'a [u8]) -> Self {
         Self(value)
+    }
+}
+
+impl<'a> From<&'a str> for BString<'a> {
+    fn from(value: &'a str) -> Self {
+        Self(value.as_bytes())
+    }
+}
+
+impl<'a> TryFrom<&BString<'a>> for &'a str {
+    type Error = Utf8Error;
+    fn try_from(value: &BString<'a>) -> Result<Self, Self::Error> {
+        from_utf8(value.0)
     }
 }
 
@@ -16,12 +35,30 @@ impl<'a> Serialize for BString<'a> {
     where
         S: Serializer,
     {
-        match from_utf8(self.0) {
+        match self.try_into() {
             Ok(string) => serializer.serialize_str(string),
-            Err(_) => {
-                let string: String = self.0.iter().map(|byte| format!("{:02x}", byte)).collect();
-                serializer.serialize_str(&string)
-            }
+            Err(_) => serializer.serialize_str(&self.to_hex_string()),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use super::BString;
+
+    #[test]
+    fn test_serialize_valid_utf8() {
+        let value = "apple";
+        let bstring = BString::from(value);
+        assert_eq!(json!(bstring), value);
+    }
+
+    #[test]
+    fn test_serialize_invalid_utf8() {
+        let value = [0xc0, 0xaf];
+        let bstring = BString::from(&value[..]);
+        assert_eq!(json!(bstring), "c0af");
     }
 }
